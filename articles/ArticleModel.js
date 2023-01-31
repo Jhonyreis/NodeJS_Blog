0355f339b62e693493d91a4f471723bf0848c38f
Jonatas Reis <studio@MacBook-Pro-de-Jonatas.local>
2023-01-31 19:58:43 -0300

    criando Models MVC (auto create tables in DB)

---
+++ articles/ArticleModel.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const conn = require('../database/database');
+
+const Article = conn.define('articles', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  body: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+});
+
+module.exports = Article;
