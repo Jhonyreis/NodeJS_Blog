ffc253d885e352ee88b98c2272821b685ab40263
Jonatas Reis <studio@MacBook-Pro-de-Jonatas.local>
2023-02-01 18:44:51 -0300

    salvando categorias no DB / criando slug com pacote slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./CategoryModel');
+const slugify = require('slugify');
 
 router.get('/admin/categories/new/', (req, res) => {
   res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+  var title = req.body.title;
+
+  //matando a aplicação em caso de conteúdo null
+  if(title == undefined) {
+    res.redirect('/admin/categories/new');
+  }
+
+  //cotinuando com o registro do DB
+  Category.create({
+    title: title,
+    slug: slugify(title, {
+      lower: true
+    })
+  }).then(() => {
+    res.redirect('/');
+  });
+
 });
 
 module.exports = router;
